96dda80e696ed0c0d2ca7abab15c0c5da26a165f
Christopher Mortensen <ChristopherMortensen@users.noreply.github.com>
2017-03-05 21:40:48 +0100

    added angular2-google-maps availability and necessary files to the project

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from 'angular2-google-maps/core';
+import { GoogleMapsComponent } from './gmaps/google-maps.component';
+
+@NgModule({
+  imports: [BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: '[Insert API key here]'
+    })],
+  declarations: [
+    AppComponent,
+    GoogleMapsComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/gmaps/google-maps.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+//import { SebmGoogleMap, SebmGoogleMapPolygon, LatLngLiteral, PolyMouseEvent } from 'angular2-google-maps/core';
+@Component({
+  selector: 'app-google-maps',
+  styles:[],
+  template: 
+  `
+  <h1>Google Maps here</h1>
+  `
+})
+export class GoogleMapsComponent implements OnInit {
+      // FUNCTIONS
+  ngOnInit() {
+    
+  }
+}
