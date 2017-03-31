97f169ea9347c7066d1994b18cd2e1dfc0a20df7
Christopher Mortensen <ChristopherMortensen@users.noreply.github.com>
2017-03-31 13:03:58 +0200

    python connect-test works

---
+++ src/python/simplecommands/connecttodrone.py
@@ -0,0 +1,26 @@
+# pylint: skip-file
+from dronekit import connect, VehicleMode
+import dronekit_sitl
+import sys
+import subprocess
+import os
+
+sitl = dronekit_sitl.start_default()
+connection_string = sitl.connection_string()
+
+vehicle = connect(connection_string, wait_ready=True)
+
+if vehicle._heartbeat_started:
+    print '0'
+else:
+    print '1'
+
+sys.stdout.flush()
+
+#Close vehicle object before exiting script
+#print "Close vehicle object"
+vehicle.close()
+
+# Shut down simulator if it was started.
+if sitl is not None:
+    sitl.stop()
---
+++ tests/basic-commands-drone.test.ts
@@ -0,0 +1,16 @@
+import test from 'ava';
+
+test.cb('Test of connect to drone()', t => {
+          
+    var spawn = require("child_process").spawn;
+    var process = spawn('python',["./src/python/simplecommands/connecttodrone.py"]);
+    var result = '';
+
+    process.stdout.on('data', function (data) {
+        result = ''+data;  
+        var arr = result.split("\n");
+        t.is(arr[arr.length - 2], "0");
+        t.end(); 
+    })
+})
+
